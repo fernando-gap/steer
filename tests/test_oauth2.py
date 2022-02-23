c462dbee459e11b15a2aaaa7d48439d16c943b52
Fernando A <fernando.augustogomesp@gmail.com>
2022-02-22 22:09:01 -0300

    test: new test version for the oauth2
    The oauth2 code base was rewritten with big changes. New tests needed to
    be written, this time these tests is more clean and concise than last
    time.

--- tests/test_oauth2.py
+++ tests/test_oauth2.py
@@ -1,156 +1,102 @@
 import unittest as ut
 from steer.oauth.api import OAuth2, _ParseParams, OAuth2CodeExchange
-
+import json
 
 class TestParseParams(ut.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        """Read all input data and outupt data for the class"""
+
+        with open('test_oauth2/fixture.json') as inputdata:
+            cls.input = json.loads(inputdata.read())
+
+        with open('test_oauth2/expected.json') as outputdata:
+            cls.output = json.loads(outputdata.read())
+
+
     def setUp(self):
-        self.parse_params = _ParseParams()
+        self.params = _ParseParams()
 
 
+    def test_scopes(self):
 
-    def test_check_scopes_equals(self):
-
-        d_ls = [
-            'https://www.googleapis.com/auth/scopes1',
-            'https://www.googleapis.com/auth/scopes2',
-            'https://www.googleapis.com/auth/scopes3',
-            'https://www.googleapis.com/auth/scopes4',
-        ]
-
-        d_res = f'?scope={d_ls[0]}%20{d_ls[1]}%20{d_ls[2]}%20{d_ls[3]}'
-        d_str = f'{d_ls[0]}%20{d_ls[1]}%20{d_ls[2]}%20{d_ls[3]}'
-
-        self.assertEqual(_ParseParams.check_scopes(d_ls), d_res)
-        self.assertEqual(_ParseParams.check_scopes(d_str), d_res)
+        # test if scope is a list
+        input_ = self.input['iscopes']['a']
+        output = self.output['oscopes']['a']
+        self.assertEqual(self.params._create_url_params(input_), output)
 
 
+        # test if scope is not a list
+        input_ = self.input['iscopes']['b']
+        output = self.output['oscopes']['b']
+        self.assertEqual(self.params._create_url_params(input_), output)
 
-    def test_check_scopes_exc_raises(self):
-        with self.assertRaises(TypeError):
-            _ParseParams.check_scopes(False)
-
-        with self.assertRaises(TypeError):
-            _ParseParams.check_scopes(True)
-
-        with self.assertRaises(TypeError):
-            _ParseParams.check_scopes(None)
-
-        with self.assertRaises(TypeError):
-            _ParseParams.check_scopes(())
-
-        with self.assertRaises(TypeError):
-            _ParseParams.check_scopes({})
-
-
-
-    def test_parse_from(self):
-        json = self.parse_params.params_from('./test_data/data.json')
-        want = ("?scope=https://www.googleapis.com/auth/scopes"
-                "&redirect_uri=http://localhost:port"
-                "&response_type=code&client_id=your_client_id")
-
-        from json import loads
-
-        self.assertEqual(json, want)
-
-        with open('./test_data/data.json') as file:
-            json = file.read()
-
-
-        sets = self.parse_params.params_from(params=loads(json))
-        self.assertEqual(sets, want)
-
-
-
-    def test_parse_exc_raises(self):
-        from json import JSONDecodeError
-        with self.assertRaises(FileNotFoundError):
-            self.parse_params.params_from('./test_data/noexist')
-
-        with self.assertRaises(TypeError):
-            self.parse_params.params_from(params=False)
-
-        with self.assertRaises(TypeError):
-            self.parse_params.params_from()
-
+    
+    def test_params(self):
+        input_ = self.params._create_url_params(self.input['iscopes']['c'])
+        output= self.output['oscopes']['c']
+        
+        self.assertEqual(input_, output)
 
 
 class TestOAuth2(ut.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.oauth2 = OAuth2(json='test_data/data.json')
+        with open('test_oauth2/fixture.json') as inputdata:
+            cls.input = json.loads(inputdata.read())
         
+        with open('test_oauth2/expected.json') as output:
+            cls.output = json.loads(output.read())
     
+
     def test_create(self):
-        result = ('https://accounts.google.com/o/oauth2/v2/auth?'
-                'scope=https://www.googleapis.com/auth/scopes'
-                '&redirect_uri=http://localhost:port'
-                '&response_type=code&client_id=your_client_id')
+        # load json config
+        auth_json = OAuth2(json_path='test_oauth2/data.json')
 
-        self.assertEqual(self.oauth2.create(), result)
+        # use a config dict instead
+        auth_dict = OAuth2(dict_params=self.input['ioauth2']['a'])
 
+        # use dictionary arguments
+        auth_args = OAuth2(client_id='client_id',
+                           response_type='code',
+                           scope='scope',
+                           redirect_uri='redirect_uri'
+                           )
+
+        output = self.output['o_auth2']['a']
+
+        self.assertEqual(auth_json.create(), output)
+        self.assertEqual(auth_dict.create(), output)
+        self.assertEqual(auth_args.create(), output)
 
     def test_accesstoken(self):
-        instance = self.oauth2.accesstoken(code="oauth_code")
-        self.assertIsInstance(instance, OAuth2CodeExchange)
+        auth = OAuth2(self.input['ioauth2']['b'])
+        output = self.output['o_auth2']['b']
+
+        self.assertEqual(auth.accesstoken('code'), output)
+
+        # test passing secret as argument
+        del auth.params['client_secret']
+        self.assertEqual(auth.accesstoken('code', 'client_secret'), output)
 
 
     def test_revokeaccess(self):
-        revoke = self.oauth2.revokeaccess(refresh_token='refresh_token')
-        result = 'https://oauth2.googleapis.com/revoke?token=' \
-                 'refresh_token'
+        auth = OAuth2(json_path='test_oauth2/data.json')
+        revoke = auth.revokeaccess(refresh_token='refresh_token')
+        result = 'https://oauth2.googleapis.com/revoke?token=refresh_token'
 
         self.assertEqual(revoke, result)
 
     
     def test_refresh_tokens(self):
-        refresh = self.oauth2.refreshtokens('secret', 'tokens')
-        
-        result = 'https://oauth2.googleapis.com/token?grant_type=refresh_token' \
-                 '&client_id=your_client_id' \
-                 '&client_secret=secret' \
-                 '&refresh_token=tokens'
+        auth = OAuth2(json_path='test_oauth2/data.json')
+        refresh_url = auth.refreshtokens('refresh_token', 'client_secret')
+        output = self.output['o_auth2']['c']
 
 
-        self.assertEqual(refresh, result)
-
-
-class TestOAuth2CodeExchange(ut.TestCase):
-
-    def test_no_code_challenge_exchange(self):
-        #test without code_verifier
-        oauth2 = OAuth2(json='./test_data/data.json')
-        oauth2.create()
-        exchange = oauth2.accesstoken('code')
-        ex = exchange.exchange(secret='secret')
-
-        result = 'https://oauth2.googleapis.com/token' \
-                 '?grant_type=authorization_code' \
-                 '&code=code' \
-                 '&client_id=your_client_id' \
-                 '&redirect_uri=http://localhost:port' \
-                 '&client_secret=secret' \
-
-        self.assertEqual(ex, result)
-
-
-    def test_code_challenge_exchange(self):
-        # test when json holds client secret
-        oauth = OAuth2(json='test_data/data-1.json')
-        oauth.create()
-        ex = oauth.accesstoken('code')
-        token = ex.exchange()
-        expected ='https://oauth2.googleapis.com/token' \
-                  '?grant_type=authorization_code' \
-                  '&code=code' \
-                  '&client_id=your_client_id' \
-                  '&redirect_uri=http://localhost:port' \
-                  '&client_secret=secret' \
-
-        self.assertEqual(token, expected)
-
+        self.assertEqual(refresh_url, output)
 
 if __name__ == '__main__':
     ut.main()
